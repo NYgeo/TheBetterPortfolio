712fd5df41ccdd97c580b2af741a23036ec93bc1
Geovani <Tzulgeo@gmail.com>
2024-11-21 09:32:21 -0500

    Ready for demo

--- assets/js/validInput.js
+++ assets/js/validInput.js
@@ -1,11 +1,5 @@
-function validateForm() {
 
     //get name
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    if(name.trim() ==="" || email.trim() === "")
-    {
-        alert("Fields cannot be empty");
-    }
 
-}
+    //if empty print error msg
+ 
